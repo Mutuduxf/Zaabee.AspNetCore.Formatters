fa70930b5559ce85883ad32ea879fba08b36687a
Mutuduxf <aeondxf@live.com>
2018-09-07 11:17:55 +0800

    Remove CanWriteResult

--- Zaabee.AspNetCore.Formatters.Jil/JilOutputFormatter.cs
+++ Zaabee.AspNetCore.Formatters.Jil/JilOutputFormatter.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Jil;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
-using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
 
 namespace Zaabee.AspNetCore.Formatters.Jil
@@ -24,11 +23,6 @@ namespace Zaabee.AspNetCore.Formatters.Jil
         {
             var response = context.HttpContext.Response;
             return response.WriteAsync(JSON.Serialize(context.Object, _jilOptions));
-        }
-
-        public override bool CanWriteResult(OutputFormatterCanWriteContext context)
-        {
-            return true;
         }
     }
 }
