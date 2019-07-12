245aaeb143987009f5712ff5a17a917a30d1ff3a
Mutuduxf <aeondxf@live.com>
2019-07-12 14:13:09 +0800

    Upgrade nuget packages.

--- Demo/TestDto.cs
+++ Demo/TestDto.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using ProtoBuf;
 
 namespace Demo
 {
+    [ProtoContract]
     public class TestDto
     {
+        [ProtoMember(1)]
         public Guid Id { get; set; }
+        [ProtoMember(2)]
         public string Name { get; set; }
+        [ProtoMember(3)]
         public DateTime CreateTime { get; set; }
+        [ProtoMember(4)]
         public List<TestDto> Kids { get; set; }
+        [ProtoMember(5)]
         public long Tag { get; set; }
+        [ProtoMember(6)]
         public TestEnum Enum { get; set; }
 //        [ProtoMember(7)] public DateTimeOffset TestTime { get; set; }
     }
